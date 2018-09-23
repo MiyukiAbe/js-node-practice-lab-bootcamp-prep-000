b6e9c7a87d17528ba7102e86d3cfbb9166358157
Miyuki Abe <miyuki0809@gmail.com>
2018-09-23 21:09:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 // var name = "Miyuki"
 // var height = 5
 
-// console.log("Name:")
-// console.log(name)
 
-// console.log("Height:")
-// console.log(height+1)
 
 
 let name = "Joe"
 let height = 74
 
+// console.log("Name:")
+// console.log(name)
 
+// console.log("Height:")
+// console.log(height+1)
 
 
 
